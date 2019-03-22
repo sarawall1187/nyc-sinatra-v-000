d531a7da132cc0f4be4124d9545debdfbf61acfb
Sara Walker <sarawall1187@gmail.com>
2019-03-22 21:23:30 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,18 +12,21 @@ class FiguresController < ApplicationController
     end
 
     post '/figures' do
-      @landmark = Landmark.find_or_create_by(params)
-      @title = Title.find_or_create_by(params)
-      @figure = Figure.create(params[:name])
-      binding.pry
-      @figure.landmark = @landmark
-      @figure.title = @title
+      @landmark = Landmark.find_or_create_by(params[:id])
+      @title = Title.find_or_create_by(params[:id])
+      @figure = Figure.create(params[:id])
+      @figure.name = params[:figure][:name]
+      @landmark.name = params[:landmark][:name]
+      @landmark.year_completed = params[:landmark][:year_completed]
+      @title = params[:title][:name]
+
       @figure.save
       redirect to '/figures/:id'
     end
 
     get '/figures/:id' do
-      @figure = Figure.find(params[:id])
+      @figure = Figure.find_by(params[:name])
+
       erb :'figures/show'
    end
 end
