e026b2c79c42bb5603271386f083aa21f7486c6d
Sara Walker <sarawall1187@gmail.com>
2019-03-23 19:53:40 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,8 +6,27 @@ class LandmarksController < ApplicationController
   end
 
   get '/landmarks/new' do
-    # @figures = Figure.all
-    @titles = Title.all
     erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
   end
 end
