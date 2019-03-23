3b0a1eb17f5e16b6b3413718f0fa0f414e08fbc4
Sara Walker <sarawall1187@gmail.com>
2019-03-23 15:07:02 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,21 +12,25 @@ class FiguresController < ApplicationController
     end
 
     post '/figures' do
-      @landmark = Landmark.find_or_create_by(params[:id])
-      @title = Title.find_or_create_by(params[:id])
-      @figure = Figure.create(params[:id])
-      @figure.name = params[:figure][:name]
-      @landmark.name = params[:landmark][:name]
-      @landmark.year_completed = params[:landmark][:year_completed]
-      @title = params[:title][:name]
-      @figure.save
+      @landmark = Landmark.find_or_create_by(:name => params[:landmark][:name])
+      @title = Title.find_or_create_by(:name => params[:title][:name])
+      @figure = Figure.create(:name => params[:figure][:name])
+
       redirect to '/figures/:id'
     end
 
     get '/figures/:id' do
-      @figure = Figure.find_by(params[:name])
-      @title = Title.find_by(params[:name])
-      @landmark = Landmark.find_by(params[:name])
+      binding.pry
+      @figure = Figure.find_by(params[:figure][:name])
+      @title = Title.find_by(params[:title][:name])
+      @landmark = Landmark.find_by(params[:landmark][:name])
       erb :'figures/show'
    end
 end
+
+# @figure.name = params[:figure][:name]
+# @landmark.name = params[:landmark][:name]
+# @landmark.year_completed = params[:landmark][:year_completed]
+# @title.name = params[:title][:name]
+# @figure.save
+# binding.pry
